481c52c8ad88c0f0fb49c08058be260c356f3236
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-22 11:06:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+tutorials = tutorials.map(function(tutorials) {
+  let count = tutorials.split(" ").length;
+  let arraySpace = tutorials.split(" ");
+  for (let i = 0; i < arraySpace.length; i++) {
+    arraySpace[i].charAt[0].toUpperCase();
+  }
+  let newTut = "";
+  for (let i = 0; i < arraySpace.length; i++) {
+    if (i == arraySpace.length - 1) {
+      newTut += newTutarraySpace[i];
+    } else {
+      newTut += newTutarraySpace[i] + " ";
+    }
+  }
+  return (tutorials = DOMQuad);
+});
 
 tutorials=tutorials.map
 const titleCased = () => {
